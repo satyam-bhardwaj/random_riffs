827e2fc79286273f236c2a322d96c233cb800af1
Satyam Bhardwaj <56554188+satyam-bhardwaj@users.noreply.github.com>
2021-03-26 01:05:02 +0100

    Update random_riffs.py

--- random_riffs.py
+++ random_riffs.py
@@ -16,7 +16,7 @@ for i, f in enumerate(rand_freq):
     #x = np.linspace(0,cycles*2*np.pi,int(cycles*44100/f))
     #t = np.arange(0,cycles/f,1/sample_rate)
     t = np.arange(0,fixed_duration,1/sample_rate)
-    #tried to replicate a fat bass tone using pure sine waves.
+    #tried to replicate a fat bass tone using combination of pure sine waves.
     sweep.append((np.sin(2*np.pi*f*t)+0.1*np.sin((5/4)*2*np.pi*f*t)+np.sin(1.5*2*np.pi*f*t)+np.sin(2*2*np.pi*f*t))**5 \
                  +0.5*(np.sin(2*np.pi*f*t)+0.1*np.sin((5/4)*2*np.pi*f*t)+np.sin(1.5*2*np.pi*f*t)-np.sin(2*2*np.pi*f*t))**5)
     #sweep.append(np.zeros(sample_rate//100))
